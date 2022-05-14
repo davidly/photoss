fd9ca0fc0a0ab820c5dd4e454358e58b32b4a092
David Lee <davidly_lee@hotmail.com>
2022-05-14 11:46:43 -0700

    build fix

--- m.bat
+++ m.bat
@@ -2,6 +2,6 @@ del *.obj
 del photoss.exe
 del photoss.pdb
 rc photoss.rc
-cl /nologo photoss.cxx /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /D_AMD64_ /link ntdll.lib user32.lib gdi32.lib photoss.res /OPT:REF /subsystem:windows
+cl /nologo photoss.cxx /I:.\ /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /D_AMD64_ /link ntdll.lib user32.lib gdi32.lib photoss.res /OPT:REF /subsystem:windows
 
 
